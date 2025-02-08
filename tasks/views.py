7a62f7ff9e01b2dfb97eec4b080567d9f2d49392
lucasbesse <lucas.bessegato@metadados.com.br>
2025-02-07 22:36:21 -0300

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all().order_by('id')
+  serializer_class = TaskSerializer
