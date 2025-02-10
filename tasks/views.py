d067a155fe0e4c73b3278086467469daa20e2ca9
lucasbesse <lucas.bessegato@metadados.com.br>
2025-02-10 09:04:23 -0300

    ordenação

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
   queryset = Task.objects.all().order_by('-id')
   serializer_class = TaskSerializer
+  filter_backends = [filters.OrderingFilter]
+  ordering_filters = ['name', '-id', 'done']
+  ordering = ['-id']
